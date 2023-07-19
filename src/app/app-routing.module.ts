9f7c38f7737e90903f55b9c4108d3a599fdcee84
Ishan Jain <ishanjain1408@gmail.com~>
2023-07-19 13:42:03 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
+import { AboutComponent } from './MyComponents/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodoItemComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
